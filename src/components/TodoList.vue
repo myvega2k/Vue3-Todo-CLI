d816414dcf951c3fd19e0910191883bc97418e89
myvega2k <vega2k@kakao.com>
2023-06-28 17:06:31 +0900

    TodoList update

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <ul>
-            <li v-for="(item,idx) in todoItems" v-bind:key="idx">
+            <li v-for="(item,idx) in todoItems" :key="idx">
                 {{ item }}
             </li>
         </ul>
@@ -26,4 +26,37 @@ onBeforeMount(() => {
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+</style>
