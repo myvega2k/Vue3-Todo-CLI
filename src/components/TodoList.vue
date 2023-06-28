1008c952177d694d91c3d6f72e00775ce0709875
myvega2k <vega2k@kakao.com>
2023-06-28 17:03:31 +0900

    TodoList update

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,29 @@
 <template>
     <div>
+        <ul>
+            <li v-for="(item,idx) in todoItems" v-bind:key="idx">
+                {{ item }}
+            </li>
+        </ul>
 
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onBeforeMount } from 'vue'
 
+const todoItems = ref<string[]>([])
+
+//life cycle hook
+onBeforeMount(() => {
+    if (localStorage.length > 0) {
+        for (var i = 0; i < localStorage.length; i++) {
+            const storageValue = localStorage.key(i) as string
+            todoItems.value.push(storageValue)
+        }
+    }
+    console.log(todoItems.value)
+})
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
