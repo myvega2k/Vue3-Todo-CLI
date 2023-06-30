92320447a8d7a714d8091c71cddc987f78b36b2d
myvega2k <vega2k@kakao.com>
2023-06-30 10:11:03 +0900

    axios add

---
+++ src/common/http-common.ts
@@ -0,0 +1,9 @@
+import axios, { AxiosInstance } from "axios";
+
+const apiClient: AxiosInstance = axios.create({
+  baseURL: "http://localhost:4500/api",
+  headers: {
+    "Content-type": "application/json",
+  },
+});
+export default apiClient;
