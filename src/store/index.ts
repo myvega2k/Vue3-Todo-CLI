4ed6cd06edf7492474de6c70fec4cbfc8db4c42b
myvega2k <vega2k@kakao.com>
2023-06-30 10:15:39 +0900

    actions property add

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,29 +1,18 @@
 import { createStore, createLogger } from "vuex";
 import TodoItem from "@/types/TodoItem";
-
-const storage = {
-  fetch() {
-    const arr = [];
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        const storageKey = localStorage.key(i) as string;
-        const itemJson = localStorage.getItem(storageKey) as string | null;
-        if (itemJson) {
-          arr.push(JSON.parse(itemJson));
-        }
-      }
-    }
-    return arr;
-  },
-};
+import http from "@/common/http-common";
 
 export type State = { todoItems: TodoItem[] };
-const state: State = { todoItems: storage.fetch() };
+const state: State = { todoItems: [] };
 
 export const store = createStore({
   plugins: process.env.NODE_ENV === "development" ? [createLogger()] : [],
   state, //state: state,
+  actions: {},
   mutations: {
+    setTodoItems(state, items) {
+      state.todoItems = items;
+    },
     addTodo(state: State, todoItem: string) {
       const obj: TodoItem = { completed: false, item: todoItem };
       localStorage.setItem(todoItem, JSON.stringify(obj));
