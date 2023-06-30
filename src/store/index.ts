7a2c32604f59f58ad4100fe499d0306d467a0ae9
myvega2k <vega2k@kakao.com>
2023-06-30 14:46:20 +0900

    modulePost add

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,16 @@
 import { createStore, createLogger } from "vuex";
+import { modulePost, ModulePostState } from "./modules/modulePost";
 import { moduleTodo, ModuleTodoState } from "./modules/moduleTodo";
 
 export interface RootState {
   moduleTodo: ModuleTodoState;
+  modulePost: ModulePostState;
 }
 
 export const store = createStore({
   plugins: process.env.NODE_ENV === "development" ? [createLogger()] : [],
   modules: {
     moduleTodo,
+    modulePost
   },
 });
--- src/store/modules/modulePost.ts
+++ src/store/modules/modulePost.ts
@@ -24,44 +24,38 @@ export const modulePost: Module<ModulePostState, RootState> = {
     }, //mutations
     getters: {
         getPosts(state) {
-            return state.posts
-                ;
+            return state.posts;
         },
         getPost(state) {
-            return state.post
-                ;
+            return state.post;
         },
     }, //getters
     actions: {
         loadPosts({ commit }) {
             http
                 .get(`/posts`)
-                .then((res) => res.data
-                )
+                .then((res) => res.data)
                 .then((items) => commit("setPosts", items))
                 .catch((err) => console.error(err));
         },
         loadPost({ commit }, payload) {
             http
                 .get(`/posts/${payload.id}`)
-                .then((res) => res.data
-                )
+                .then((res) => res.data)
                 .then((item) => commit("setPost", item))
                 .catch((err) => console.error(err));
         },
         removePost({ commit }, id) {
             http
                 .delete(`/posts/${id}`)
-                .then((res) => res.data
-                )
+                .then((res) => res.data)
                 .then((items) => commit("setPosts", items))
                 .catch((err) => console.error(err));
         },
         addPost({ commit }, payload) {
             http
                 .post(`/posts`, payload)
-                .then((res) => res.data
-                )
+                .then((res) => res.data)
                 .then((items) => commit("setPosts", items))
                 .catch((err) => console.error(err));
         },
